08b5845485b5e533ce060fb6c3ecebb85ce96fff
marcosIrazoquiITEC <m.irazoqui@itecriocuarto.org.ar>
2024-06-05 21:16:33 -0300

    header y primeras config

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import MainLayout from "../layouts/MainLayout";
+
+export default function Home() {
+    
+      return  (
+        <MainLayout>
+            <h2> ACA VA EL COMPONENTE CARROUSEL </h2>
+            <h2> ACA COMPONENTES CARDS O ALGO ASI</h2>
+        </MainLayout>
+        ) 
+}
