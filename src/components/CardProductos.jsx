00205db5ac7ebd33eecde98d15348c710cbfb5b8
marcosIrazoquiITEC <m.irazoqui@itecriocuarto.org.ar>
2024-06-17 17:16:26 -0300

    fetch usd develop

--- src/components/CardProductos.jsx
+++ src/components/CardProductos.jsx
@@ -69,7 +69,7 @@ export default function CardProductos({ info }) {
             </div>
             <div className="col-3 flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
             
-              <span className="mt-5 text-2xl font-semibold">USD {numeral(product.price/Dolar).format("0,0.00").replace(/,/g, '#').replace(/\./g, ',').replace(/#/g, '.')}</span>
+              <span className="mt-5 text-2xl font-semibold">$ {numeral(product.price).format("0,0.00").replace(/,/g, '#').replace(/\./g, ',').replace(/#/g, '.')}</span>
               <Button label='Agregar al carrito'  onClick={handleBotonCarrito} icon="pi pi-plus" className="p-button-rounded mt-1" ></Button>
             </div>
           </div>
